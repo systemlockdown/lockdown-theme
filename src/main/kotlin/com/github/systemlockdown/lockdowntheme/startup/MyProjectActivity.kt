096063bd9a9c44c9a0dffaf8bbd61eb62b9b5780
GitHub Action <action@github.com>
2025-09-07 09:09:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/systemlockdown/lockdowntheme/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/systemlockdown/lockdowntheme/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.systemlockdown.lockdowntheme.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
